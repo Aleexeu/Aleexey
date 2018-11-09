d3dbd9219d4d2b4903c0e207ab46ad3ed7cd5de1
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-09 15:20:36 -0200

    Update sugestão.js

--- comandos/sugestão.js
+++ comandos/sugestão.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
     let suggestmessage = args.join(" ").slice(0);
-    let suggestchannel = message.guild.channels.find(c => c.name == "sugestão");
+    let suggestchannel = message.guild.channels.find(c => c.name == "💧sᴜɢᴇsᴛᴏᴇs");
 
     if (!suggestmessage) {
         return message.reply("Porfavor, insira sua sugestão!")
